c490acbf717fe8f832bbc6a7f498deb928918980
skjeng <skjeng@ntu.edu.tw>
2015-06-16 20:27:58 +0800

    skjGH 06162015

--- GithubTeamWorkDemo/Program.cs
+++ GithubTeamWorkDemo/Program.cs
@@ -2,12 +2,17 @@
 
 namespace GithubTeamWorkDemo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("skjGH 06132015");
             Console.WriteLine("skj1957 06142015");
+        }
+
+        public  static void Print_skjGH_06162015()
+        {
+            Console.WriteLine("skjGH 06162015");
         }
     }
 }
---
+++ UnitTestProject1_skjGH/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GithubTeamWorkDemo;
+
+namespace UnitTestProject1_skjGH
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program.Print_skjGH_06162015();
+            Assert.IsTrue(true);
+        }
+    }
+}
